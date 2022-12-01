aa9a28dc7deef0d5b486b92366cbcacbb62dd0e4
AugustHjortholm <s215800@dtu.dk>
2022-12-01 22:38:54 +0100

    Initial Setup of structure

---
+++ app/src/main/java/com/example/s215800_wheeloffortune/StartGameScreen.kt
@@ -0,0 +1,16 @@
+package com.example.s215800_wheeloffortune
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StartGameScreen(
+) {
+
+}
+
+@Preview (showBackground = true)
+@Composable
+fun StartGameScreenPreview(){
+    StartGameScreen()
+}
---
+++ app/src/main/java/com/example/s215800_wheeloffortune/WheelOfFortuneApp.kt
@@ -0,0 +1,20 @@
+package com.example.s215800_wheeloffortune
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun WheelOfFortuneApp(){
+
+    val navController = rememberNavController()
+
+    //TODO
+    NavHost(
+        navController = navController,
+        startDestination = "start_screen"
+    ) {
+        composable("start_screen") { StartGameScreen() }
+    }
+}
